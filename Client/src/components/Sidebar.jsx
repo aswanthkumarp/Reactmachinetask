e0243bfca35f2ef223b5dcb4c1b474db44c1811b
Aswanth Kumar P <aswanthkumar777@gmail.com>
2024-01-13 01:48:18 +0530

    completed sidebar component

---
+++ Client/src/components/Sidebar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Logo from '../assets/Briefcase.png';
+import StatBoard from '../assets/StatBoard.png';
+import SideBarLinks from './SideBarLinks';
+import CircleMenu from '../assets/Circled Menu.png';
+import Support from '../assets/Support.png';
+import Plugin from '../assets/Puzzle.png';
+import Help from '../assets/Help.png';
+import Logout from '../assets/Shutdown.png';
+
+const Sidebar = () => {
+  return (
+    <div className='bg-[#4C566A] flex flex-col w-52 h-screen'>
+      <div className='flex flex-col justify-center items-center p-4'>
+        <img src={Logo} alt='Logo' className='h-16 w-16 mb-2' />
+        <img src={StatBoard} alt='Title' className=' w-auto' />
+      </div>
+      <div className='mt-28  mr-1 flex flex-col gap-6 '>
+        <SideBarLinks
+          logo={CircleMenu}
+          title={'Dashboard'}
+          isDashboard={true}
+        />
+        <SideBarLinks logo={Support} title={'Support'} />
+        <SideBarLinks logo={Plugin} title={'Plugin'} />
+        <SideBarLinks logo={Help} title={'Help'} />
+      </div>
+      <div className='flex items-center mt-auto justify-center bg-white gap-2   py-2'>
+        <p className=' text-lg text-red-500 '>Logout</p>
+        <img src={Logout} alt='logout' />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ Client/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/Sidebar";
+
+
+const Dashboard = () => {
+  return (
+    <div className='flex'>
+<Sidebar/>
+    </div>
+  );
+};
+
+export default Dashboard;
