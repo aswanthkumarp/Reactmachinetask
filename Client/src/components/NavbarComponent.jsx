a8c35b9fced8cca2f03d65137b134d349133cbf3
Aswanth Kumar P <aswanthkumar777@gmail.com>
2024-01-13 14:10:35 +0530

    added navbar component

--- Client/src/components/NavbarComponent.jsx
+++ Client/src/components/NavbarComponent.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import UserImg from '../assets/Rectangle 10.png';
 
 const NavbarComponent = () => {
   return (
-    <div>NavbarComponent</div>
-  )
-}
+    <div className='flex bg-transparent mt-5 '>
+      <div className='flex ml-8 justify-center items-center'>
+        <p className='text-sm font-semibold'>Good Morning!</p>
+        <p>🌞</p>
+      </div>
+      <div className='flex bg-white roundend-lg shadow-md ml-auto mr-2 justify-between p-1 rounded-lg gap-2'>
+        <div className='flex flex-col '>
+          <p className='text-sm font-semibold'>John Doe</p>
+          <p className='text-basic'>john@doe.com</p>
+        </div>
+        <div className='py-3 w-25 h-25 '>
+          <img
+            src={UserImg}
+            alt='userimg'
+            className='h-full w-full object-contain'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NavbarComponent
+export default NavbarComponent;
