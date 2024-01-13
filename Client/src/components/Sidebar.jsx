f3c99d143b1c733135dce9b4f5c5196c87722567
Aswanth Kumar P <aswanthkumar777@gmail.com>
2024-01-14 01:38:42 +0530

    made changes to the sidebar and added responsiveness

--- Client/src/components/Sidebar.jsx
+++ Client/src/components/Sidebar.jsx
@@ -1,38 +1,121 @@
 import React from 'react';
-import Logo from '../assets/Briefcase.png';
+import {
+  IconButton,
+  List,
+  ListItem,
+  ListItemPrefix,
+  Drawer,
+  Card,
+} from '@material-tailwind/react';
+
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import LogoImg from '../assets/Briefcase.png';
 import StatBoard from '../assets/StatBoard.png';
-import SideBarLinks from './SideBarLinks';
 import CircleMenu from '../assets/Circled Menu.png';
 import Support from '../assets/Support.png';
 import Plugin from '../assets/Puzzle.png';
 import Help from '../assets/Help.png';
 import Logout from '../assets/Shutdown.png';
 
-const Sidebar = () => {
-  return (
-    <div className='relative flex h-screen flex-col bg-[#4C566A]'>
-      <div className='bg-[#4C566A] flex flex-col w-52 h-screen'>
-        <div className='flex flex-col justify-center items-center p-4'>
-          <img src={Logo} alt='Logo' className='h-16 w-16 mb-2' />
-          <img src={StatBoard} alt='Title' className=' w-auto' />
-        </div>
-        <div className='mt-28  mr-1 flex flex-col gap-6 '>
-          <SideBarLinks
-            logo={CircleMenu}
-            title={'Dashboard'}
-            isDashboard={true}
-          />
-          <SideBarLinks logo={Support} title={'Support'} />
-          <SideBarLinks logo={Plugin} title={'Plugin'} />
-          <SideBarLinks logo={Help} title={'Help'} />
-        </div>
-      </div>
-      <div className='flex items-center mt-auto justify-center bg-white h-5 '>
-        <p className=' text-lg text-red-500 '>Logout</p>
-        <img src={Logout} alt='logout' />
-      </div>
-    </div>
-  );
-};
+export default function SidebarWithBurgerMenu() {
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
 
-export default Sidebar;
+  const openDrawer = () => setIsDrawerOpen(true);
+  const closeDrawer = () => setIsDrawerOpen(false);
+
+  return (
+    <>
+      <div className='lg:hidden '>
+        <IconButton variant='text' size='lg' onClick={openDrawer}>
+          {isDrawerOpen ? (
+            <XMarkIcon className='h-8 w-8 stroke-2' />
+          ) : (
+            <Bars3Icon className='h-8 w-8 stroke-2' />
+          )}
+        </IconButton>
+      </div>
+      <div className='hidden lg:flex flex-col  top-0 h-screen bg-[#3B4252] sticky '>
+        <Card color='transparent' shadow={false} className=' h-full w-full '>
+          <div className='mb-2 flex flex-col items-center gap-4 p-4'>
+            <img src={LogoImg} alt='brand' className='h-25 w-25' />
+            <img src={StatBoard} alt='brand' className='h-25 w-25' />
+          </div>
+
+          <List className='mt-20 gap-5 flex-grow '>
+            <ListItem className='bg-white'>
+              <ListItemPrefix>
+                <img src={CircleMenu} alt='Dashboard' />
+              </ListItemPrefix>
+              Dashboard
+            </ListItem>
+            <ListItem className='bg-[#2E3440] text-white'>
+              <ListItemPrefix>
+                <img src={Support} alt='Support' />
+              </ListItemPrefix>
+              Support
+            </ListItem>
+            <ListItem className='bg-[#2E3440] text-white'>
+              <ListItemPrefix>
+                <img src={Plugin} alt='Plugin' />
+              </ListItemPrefix>
+              Plugins
+            </ListItem>
+            <ListItem className='bg-[#2E3440] text-white'>
+              <ListItemPrefix>
+                <img src={Help} alt='Help' />
+              </ListItemPrefix>
+              Help
+            </ListItem>
+          </List>
+          <div className='bg-white mt-auto w-full p-4 text-center flex gap-2 justify-center '>
+            <p className='text-red '>Logout</p>
+            <img src={Logout} alt='' />
+          </div>
+        </Card>
+      </div>
+      <Drawer
+        open={isDrawerOpen}
+        onClose={closeDrawer}
+        className='bg-[#3B4252]'
+      >
+        <Card color='transparent' shadow={false} className=' h-full w-full '>
+          <div className='mb-2 flex flex-col items-center gap-4 p-4'>
+            <img src={LogoImg} alt='brand' className='h-25 w-25' />
+            <img src={StatBoard} alt='brand' className='h-25 w-25' />
+          </div>
+
+          <List className='mt-20 gap-5 flex-grow '>
+            <ListItem className='bg-white'>
+              <ListItemPrefix>
+                <img src={CircleMenu} alt='Dashboard' />
+              </ListItemPrefix>
+              Dashboard
+            </ListItem>
+            <ListItem className='bg-[#2E3440] text-white'>
+              <ListItemPrefix>
+                <img src={Support} alt='Support' />
+              </ListItemPrefix>
+              Support
+            </ListItem>
+            <ListItem className='bg-[#2E3440] text-white'>
+              <ListItemPrefix>
+                <img src={Plugin} alt='Plugin' />
+              </ListItemPrefix>
+              Plugins
+            </ListItem>
+            <ListItem className='bg-[#2E3440] text-white'>
+              <ListItemPrefix>
+                <img src={Help} alt='Help' />
+              </ListItemPrefix>
+              Help
+            </ListItem>
+          </List>
+          <div className='bg-white mt-auto w-full p-4 text-center flex gap-2 justify-center '>
+            <p className='text-red '>Logout</p>
+            <img src={Logout} alt='' />
+          </div>
+        </Card>
+      </Drawer>
+    </>
+  );
+}
